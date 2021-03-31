0de51b6baf2e83805e12c7878d04d4ee632edf48
Luis Mayta <luis@hadenlabs.com>
2021-03-31 11:16:38 -0500

    test: implement validation generate secrets (#5)

---
+++ test/secrets_with_visibility_all_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestSecretWithVisibilityAll(t *testing.T) {
+	t.Parallel()
+
+	// Set config settings for the secret this test should create
+	secrets := map[string]interface{}{
+		"MYSECRET": "42",
+	}
+
+	terraformOptions := &terraform.Options{
+		// The path to where your Terraform code is located
+		TerraformDir: "secrets-with-visibility",
+		Upgrade:      true,
+		Vars: map[string]interface{}{
+			"visibility": "all",
+			"secrets":    secrets,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+}
