c5dd707c7b7cc72c2c74c6d588628a4abaa9a877
Luis Mayta <luis@hadenlabs.com>
2021-03-30 20:47:09 -0500

    perf: validation vars visibility (#3)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "secrets" {
-  description = "output instance github actions secrets"
+  description = "output instance github actions secrets organization"
   value       = github_actions_organization_secret.this
 }
