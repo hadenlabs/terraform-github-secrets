5dd5090363dc5dfa75dcffe2182478a63fb1b088
@slovacus <slovacus@gmail.com>
2021-03-31 18:21:29 -0500

    chore: implement phony to make (#5)

--- provision/make/terraform.mk
+++ provision/make/terraform.mk
@@ -1,6 +1,5 @@
-## Terraform
+## Terraform show help commands
 .PHONY: terraform.help
-
 terraform.help:
 	@echo '    terraform:'
 	@echo ''
@@ -8,12 +7,14 @@ terraform.help:
 	@echo '        terraform.docs           generate documentation variables'
 	@echo ''
 
+## Terraform show help commands
+.PHONY: terraform
 terraform:
 	make terraform.help
 
-
+## Terraform generate docs
+.PHONY: terraform.docs
 terraform.docs:
 	$(call terraform-docs, ${TERRAFORM_README_FILE}, \
 			'This document gives an overview of variables used in the platform of the ${PROJECT}.', \
 			variables.tf)
-.PHONY: terraform.docs
