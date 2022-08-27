8f4916793f6c6084bac9ec767e9c5da5e257174f
Luis Mayta <luis@hadenlabs.com>
2022-08-27 08:21:10 -0300

    feat: implement configuration and interface (#14)

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"path/filepath"
+	"runtime"
+
 	"github.com/hadenlabs/terraform-github-secrets/internal/errors"
 	"github.com/hadenlabs/terraform-github-secrets/internal/version"
 )
@@ -10,13 +13,11 @@ type Config struct {
 	App App
 }
 
-// Configurer methods for config.
-type Configurer interface {
-	ReadConfig() (*Config, error)
-}
-
 // ReadConfig read values and files for config.
 func (c *Config) ReadConfig() (*Config, error) {
+	_, filePath, _, _ := runtime.Caller(0)
+	rootDir := filepath.Dir(filePath)
+	c.App.RootPath = filepath.Join(rootDir, "..")
 	tag := version.Short()
 	c.App.Version = tag
 	return c, nil
